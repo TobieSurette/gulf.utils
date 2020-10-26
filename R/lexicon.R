e5fd4ecc30472bf189547417f524df891379fafb
Tobie Surette <tobie.surette@gmail.com>
2020-10-26 12:40:57 -0300

    Update lexicon.R

--- R/lexicon.R
+++ R/lexicon.R
@@ -1,11 +1,13 @@
-#' Build Lexicon
+#' Extract Lexicon
 #'
-#' @description Returns the different words used in a an object with text.
+#' @description Returns the different words used in a an object within text.
 #'
 #' @param x Character vector or an object coercible to a text objexct.
 #' @param file Character vector specifying a file to be read.
 #' @param ... Further arguments (not used).
 #'
+#' @examples
+#' lexicon("The punctuation, in this phrase; is ... really: 'terrible'.")
 
 #' @export lexicon
 lexicon <- function(x, ...) UseMethod("lexicon")
@@ -16,4 +18,8 @@ lexicon.default <- function(x, file, ...) if (!missing(file)) return(lexicon(rea
 
 #' @describeIn lexicon Returns a character vector of component words.
 #' @export
-lexicon.character <- function(x, ...) return(unique(unlist(strsplit(x, "[ .:();]"))))
+lexicon.character <- function(x, ...){
+   v <- unique(unlist(strsplit(x, "[ ,-.:'();]")))
+   v <- v[nchar(v) > 0]
+   return(v)
+}
