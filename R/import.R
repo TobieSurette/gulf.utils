2d6c2797ad8dd2e6248688ad946b58a84f71b448
TobieSurette <tobie.surette@gmail.com>
2021-03-17 11:54:25 -0300

    Corrected error with import function

--- R/import.R
+++ R/import.R
@@ -1,17 +1,17 @@
 #' Import Data
-#' 
+#'
 #' @description Import from one object into another.
-#' 
+#'
 #' @param x Target object.
 #' @param value Object containing catch data to be assigned.
 #' @param by Character string(s) specifying which variables use as an index key.
 #' @param variables Character string(s) specifying the fields to import from the source object.
 #' @param fill.na Value to be used to fill in target field entries with no matching source entries.
 #' @param ... Other parameters (not used).
-#' 
+#'
 #' @seealso \code{\link[gulf.utils]{match}}
-#' 
-#' @examples 
+#'
+#' @examples
 #' x <- read.scsset(year = 2020, valid = 1)
 #' b <- read.scsbio(2020)
 #' import(x, fill = 0) <- catch(b, category = c("M", "COM", "MF"))
@@ -24,18 +24,19 @@
 #' @export
 "import<-.data.frame" <- function(x, value, variables, fill.na, ...){
    if (missing(variables)) variables <- names(value)
-   
+
+   # Define indexing variables:
+   key <- c(list(...)$by, list(...)$key, key(value), key(x))
+   if (is.null(key)) key <- intersect(variables, names(x))
+
    # Remove indexing variables:
-   variables <- setdiff(variables, list(...)$by)
-   variables <- setdiff(variables, list(...)$key)
-   variables <- setdiff(variables, key(value))
-   variables <- setdiff(variables, key(x))
+   variables <- setdiff(variables, key)
    variables <- variables[variables %in% names(value)]
-   if (length(variables) == 0) stop("No matching variables to import from source object.") 
+   if (length(variables) == 0) stop("No matching variables to import from source object.")
 
    # Append results:
    if (length(variables) > 0){
-      ix <- gulf.utils::match(x, value, ...)
+      ix <- gulf.utils::match(x, value, by = key, ...)
       x[variables] <- NA
       x[variables] <- value[ix, variables]
    }
@@ -46,6 +47,6 @@
       tmp[is.na(tmp)] <- fill.na
       x[variables] <- tmp
    }
-   
+
    return(x)
 }
