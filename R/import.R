21f8574d64acbb567d491e1b272f31f1ecc26066
Tobie Surette <tobie.surette@gmail.com>
2021-03-19 16:53:45 -0300

    Added groupijng variable to import function

--- R/import.R
+++ R/import.R
@@ -12,6 +12,13 @@
 #' @seealso \code{\link[gulf.utils]{match}}
 #'
 #' @examples
+#' # Import snow crab survey by-catch data:
+#' x <- read.scsset(year = 2020, valid = 1)
+#' y <- read.scscat(year = 2020, species = c("cod", "plaice"))
+#' y$species <- species(y$species)
+#' import(x, fill = 0, var = "number.caught", group = "species") <- y
+#'
+#' # Import snow crab category data:
 #' x <- read.scsset(year = 2020, valid = 1)
 #' b <- read.scsbio(2020)
 #' import(x, fill = 0) <- catch(b, category = c("M", "COM", "MF"))
@@ -22,7 +29,24 @@
 
 #' @describeIn import-set Import assignment method for data frames.
 #' @export
-"import<-.data.frame" <- function(x, value, variables, fill.na, ...){
+"import<-.data.frame" <- function(x, value, variables, fill.na, group, ...){
+
+   # Parse 'group' argument:
+   if (!missing(group)){
+      if (is.character(group)){
+         if ((group %in% names(value)) & length(group) == 1){
+            group <- value[group]
+         }else{
+            group <- NULL
+         }
+      }
+      value <- value[setdiff(names(value), names(group))]
+      if (!is.null(group)) group <- as.data.frame(group)
+   }else{
+      group <- NULL
+   }
+
+   # Define variable to merge:
    if (missing(variables)) variables <- names(value)
 
    # Define indexing variables:
@@ -36,16 +60,38 @@
 
    # Append results:
    if (length(variables) > 0){
-      ix <- gulf.utils::match(x, value, by = key, ...)
-      x[variables] <- NA
-      x[variables] <- value[ix, variables]
-   }
+      if (is.list(group)){
+         g <- unique(group[,1])
+         if (length(g) > 0){
+            na.var <- NULL
+            for (i in 1:length(g)){
+               for (j in 1:length(variables)){
+                  v <- value[which(group[,1] == g[i]), ]
+                  if (length(v) > 0){
+                     ix <- gulf.utils::match(x, v, by = key, ...)
+                     str <- as.character(g[i])
+                     if (length(variables) > 1) str <- paste0(str, ".", variables[j])
+                     str <- gsub(" ", ".", str)
+                     x[str] <- NA
+                     x[str] <- v[ix, variables[j]]
+                     na.var <- c(na.var, str)
+                  }
+               }
+            }
+         }
+      }else{
+         ix <- gulf.utils::match(x, value, by = key, ...)
+         x[variables] <- NA
+         x[variables] <- value[ix, variables]
+         na.var <- variables
+      }
 
-   # Fill NA values:
-   if (!missing(fill.na)){
-      tmp <- x[variables]
-      tmp[is.na(tmp)] <- fill.na
-      x[variables] <- tmp
+      # Fill NA values:
+      if (!missing(fill.na)){
+         tmp <- x[na.var]
+         tmp[is.na(tmp)] <- fill.na
+         x[na.var] <- tmp
+      }
    }
 
    return(x)
